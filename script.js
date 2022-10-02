ff5a2f5e097cd4a79913200c592e1ce958543749
Richard Radcliffe <radcliffer94@gmail.com>
2022-10-02 13:42:58 -0700

    Associate game board to array

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 (function (){
-    let boardSize = 3
+    let boardSize = 3;
     cacheDom = function () {
         this.gridBox = document.querySelector(".gridBox");
     }
-
-    makeBoard = function() {
+    makeBoardGridValues = function() {
+        let board = [];
         for (let i = 0; i<boardSize; i++){
+            board[i] = [];
+            for (let j = 0; j<boardSize; j++){
+                board[i][j] = null
+            }
+        }
+        return board;
+    }
+    displayBoard = function(board) {
+        for (let i = 0; i<board.length; i++){
             const column = document.createElement("div");
             column.classList.add("column");
             this.gridBox.appendChild(column);
-            console.log(this.gridBox)
-            for (let j = 0; j <boardSize; j++){
+            for (let j = 0; j<board.length; j++){
                 const row = document.createElement("div");
                 row.classList.add("row");
+                row.textContent = board[i][j]
+                row.addEventListener("click", markBoard)
                 column.appendChild(row);
             }
-
         }
     }
+    function markBoard() {
+        console.log("Hi")
+
+    }
+
+
 
     cacheDom()
-    makeBoard()
+    board = makeBoardGridValues()
+    displayBoard(board)
 })()
+
