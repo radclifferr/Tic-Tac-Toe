57a6b9389357348f6cc74be9f2d5548690abf8fe
Richard Radcliffe <radcliffer94@gmail.com>
2022-10-12 12:02:16 -0700

    Display game board relation to grid array
    Display the game board array on screen and when the
    user clicks on a certain portion of the game board,
    the dataset array updates and subsequently regenerates the display

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+const gridBox = document.querySelector(".grid-box");
+const xChoice = document.getElementById("x");
+const oChoice = document.getElementById("o");
+
+JSON.stringify
 function getPlayerChoice () {
-    const xChoice = document.getElementById("x");
-    const oChoice = document.getElementById("o");
     xChoice.addEventListener("click", () => {
         storePlayerChoice("X")
         returnPlayerChoices("X")
     });
-
     oChoice.addEventListener("click", () => {
         storePlayerChoice("O")
         returnPlayerChoices("O")
@@ -37,39 +39,49 @@ function CreateGridArray () {
         for (let i = 0; i<this.boardSize; i++){
             this.board[i] = [];
             for (let j = 0; j<this.boardSize; j++){
-                this.board[i][j] = null
+                this.board[i][j] = null;
             }
         }
         return this.board;
     }
 }
+
 const gameBoardDataArray = new CreateGridArray().make()
 
+displayBoard = function() {
+    gridBox.replaceChildren();
+    for (let i = 0; i<gameBoardDataArray.length; i++){
+        const column = document.createElement("div");
+        column.classList.add("column");
+        gridBox.appendChild(column);
+        for (let j = 0; j<gameBoardDataArray.length; j++){
+            const row = document.createElement("div");
+            row.classList.add("row");
+            column.appendChild(row);
+            row.textContent = gameBoardDataArray[j][i]
+            row.dataset.index = `${j},${i}`
+            row.addEventListener("click", () => {
+                markArray(row.dataset.index.split(","))
+            })
 
-    // displayBoard = function(board) {
-    //     for (let i = 0; i<board.length; i++){
-    //         const column = document.createElement("div");
-    //         column.classList.add("column");
-    //         this.gridBox.appendChild(column);
-    //         for (let j = 0; j<board.length; j++){
-    //             const row = document.createElement("div");
-    //             row.classList.add("row");
-    //             column.appendChild(row);
+        }
+    }
+}
 
+displayBoard()
+let playerTurn="player1";
+function markArray(dataIndex, playerTurn) {
+    console.log(dataIndex)
+    if (playerTurn === "player1") {
+        gameBoardDataArray[dataIndex[0]][dataIndex[1]] = "X"
+        console.log(gameBoardDataArray)
+    }else {
+        gameBoardDataArray[dataIndex[0]][dataIndex[1]] = "O"
 
-
-
-    //             row.textContent = board[i][j]
-    //             row.dataset.index = `${i},${j}`
-    //             row.addEventListener("click", () => {
-    //                 markArray(row.dataset.index.split(","))
-    //                 markGameBoardDisplay(row,i,j)
-    //             })
-
-
-
-
-
+        displayBoard()
+        console.log(gameBoardDataArray)
+    }
+}
 
 
 
@@ -80,20 +92,14 @@ const gameBoardDataArray = new CreateGridArray().make()
 // //     let boardSize = 3;
 // //     this.playerTurn = "player1"
 // //     cacheDom = function () {
-// //         this.gridBox = document.querySelector(".gridBox");
+// //         this.
 // //     }
 
 // //     }
 
 
      
-//     // function markArray(datasetIndex) {
-//     //     if (this.playerTurn === "player1") {
-//     //         board[datasetIndex[0]][datasetIndex[1]] = "X"
-//     //     }else {
-//     //         board[datasetIndex[0]][datasetIndex[1]] = "O"
-//     //     }
-//     // }
+
 //     // function markGameBoardDisplay (row,i,j) {
 //     //     row.textContent = board[i][j]
 //     //     this.row = document.querySelectorAll (".row")
